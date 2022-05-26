5711f583d353d5d984eea5264fd485603f7c3555
Abraham Naiborhu <87258755+abrahamparn@users.noreply.github.com>
2022-05-26 13:29:47 +0700

    Adding cameraX capability to rice feature. Adding utils file to support the rice plant picture taking capability

--- app/src/main/java/com/example/tanamin/ui/mainfeature/riceplant/RicePlantActivity.kt
+++ app/src/main/java/com/example/tanamin/ui/mainfeature/riceplant/RicePlantActivity.kt
@@ -1,22 +1,105 @@
 package com.example.tanamin.ui.mainfeature.riceplant
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.tanamin.databinding.ActivityRicePlantBinding
+import java.io.File
 
 class RicePlantActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRicePlantBinding
+
+    companion object {
+        const val CAMERA_X_RESULT = 200
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private const val REQUEST_CODE_PERMISSIONS = 10
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (!allPermissionsGranted()) {
+                Toast.makeText(
+                    this,
+                    "Tidak mendapatkan permission.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
+            }
+        }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRicePlantBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //CHECKING CAMERA PERMISSION
+        if (!allPermissionsGranted()) {
+            ActivityCompat.requestPermissions(
+                this,
+                REQUIRED_PERMISSIONS,
+                REQUEST_CODE_PERMISSIONS
+            )
+        }
+
+        binding.cameraXButton.setOnClickListener { startCameraX() }
 
         //Handling Backbutton
         val actionbar = supportActionBar
         actionbar!!.title = "TANAMIN"
         actionbar.setDisplayHomeAsUpEnabled(true)
         actionbar.setDisplayHomeAsUpEnabled(true)
+    }
+
+    private fun startCameraX() {
+        val intent = Intent(this, CameraRiceActivity::class.java)
+        launcherIntentCameraX.launch(intent)
+    }
+
+    private val launcherIntentCameraX = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == CAMERA_X_RESULT) {
+            val myFile = it.data?.getSerializableExtra("picture") as File
+            val isBackCamera = it.data?.getBooleanExtra("isBackCamera", true) as Boolean
+            val result = rotateBitmap(
+                BitmapFactory.decodeFile(myFile.path),
+                isBackCamera
+            )
+
+            binding.previewImageView.setImageBitmap(result)
+        }
+    }
+
+    fun rotateBitmap(bitmap: Bitmap, isBackCamera: Boolean = false): Bitmap {
+        val matrix = Matrix()
+        return if (isBackCamera) {
+            matrix.postRotate(90f)
+            Bitmap.createBitmap(bitmap, 0, 0, bitmap.width ,bitmap.height, matrix,  true)
+        } else {
+            matrix.postRotate(-90f)
+            matrix.postScale(-1f, 1f, bitmap.width / 2f, bitmap.height / 2f) // flip gambar
+            Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
+        }
     }
 
     //Handling onBackPressed for the Backbutton
