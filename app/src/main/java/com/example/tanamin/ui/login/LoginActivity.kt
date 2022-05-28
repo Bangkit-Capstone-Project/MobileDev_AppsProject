04ede7b517aafa3fa6212cc6c09f5d9045fbc478
Abraham Naiborhu <87258755+abrahamparn@users.noreply.github.com>
2022-05-28 12:40:57 +0700

    Updating the Login and register UI so that the password would be hidden when user try to input it

--- app/src/main/java/com/example/tanamin/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/tanamin/ui/login/LoginActivity.kt
@@ -27,7 +27,6 @@ class LoginActivity : AppCompatActivity() {
             intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
             startActivity(intent)
         }
-
     }
 
     //Handling onBackPressed for the Backbutton
