f5c1df61702eb408989832dc1450549d1148a631
Abraham Naiborhu <87258755+abrahamparn@users.noreply.github.com>
2022-06-02 17:37:41 +0700

    adding register capability

--- app/src/main/java/com/example/tanamin/nonui/api/ApiService.kt
+++ app/src/main/java/com/example/tanamin/nonui/api/ApiService.kt
@@ -1,4 +1,19 @@
 package com.example.tanamin.nonui.api
 
+import com.example.tanamin.nonui.response.RegisterResponse
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
 interface ApiService {
+    //REGISTERING
+    @FormUrlEncoded
+    @POST("users")
+    fun registerUser(
+        @Field("email") email: String,
+        @Field("username") username: String,
+        @Field("password") password: String,
+        @Field("fullname") fullname: String,
+        ): Call<RegisterResponse>
 }
---
+++ app/src/main/java/com/example/tanamin/nonui/response/RegisterResponse.kt
@@ -0,0 +1,12 @@
+package com.example.tanamin.nonui.response
+
+import com.google.gson.annotations.SerializedName
+
+data class RegisterResponse(
+
+	@field:SerializedName("message")
+	val message: String,
+
+	@field:SerializedName("status")
+	val status: String
+)
